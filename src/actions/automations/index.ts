f69a34ad8015d97a4362ddb514d7438b8ac3e499
Sri Sudersan <tgsuder@gmail.com>
2024-12-25 23:16:29 -0700

    Added Tanstack and mutation hooks for automations

--- src/actions/automations/index.ts
+++ src/actions/automations/index.ts
@@ -1,12 +1,12 @@
 "use server";
 
 import { onCurrentUser } from "../user";
-import { createAutomation } from "./queries";
+import { createAutomation, getAutomations } from "./queries";
 
-export const getAllAutomations = async () => {
+export const createAutomations = async (id?: string) => {
   const user = await onCurrentUser();
   try {
-    const create = await createAutomation(user.id);
+    const create = await createAutomation(user.id, id);
     if (create) {
       return {
         status: 200,
@@ -15,5 +15,18 @@ export const getAllAutomations = async () => {
     }
   } catch (error) {
     return { status: 500, data: "Internal Server Error" };
+  }
+};
+
+export const getAllAutomations = async () => {
+  const user = await onCurrentUser();
+  try {
+    const automations = await getAutomations(user.id);
+
+    if (automations) return { status: 200, data: automations.automations };
+
+    return { status: 404, data: [] };
+  } catch (error) {
+    return { status: 500, data: [] };
   }
 };
--- src/actions/automations/queries.ts
+++ src/actions/automations/queries.ts
@@ -1,13 +1,32 @@
 import { client } from "@/lib/prisma";
 
-export const createAutomation = async (clerkId: string) => {
+export const createAutomation = async (clerkId: string, id?: string) => {
   return await client.user.update({
     where: {
       clerkId,
     },
     data: {
       automations: {
-        create: {},
+        create: {
+          ...(id && { id }),
+        },
+      },
+    },
+  });
+};
+
+export const getAutomations = async (clerkId: string) => {
+  return await client.user.findUnique({
+    where: { clerkId },
+    select: {
+      automations: {
+        orderBy: {
+          createdAt: "asc",
+        },
+        include: {
+          keywords: true,
+          listener: true,
+        },
       },
     },
   });
